3d14b4e1fa1403d363cbe8b2937e06da3e8fa6f9
Neha Verma <nehaverma1997@gmail.com>
2022-12-23 10:35:41 +0530

    Base URL check

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 require("./routes/user")(app);
 
-app.get("/api/v1/liveness", function (req, res) {
+app.get("/", function (req, res) {
     res.send("Welcome to Assignment");
 });
 
